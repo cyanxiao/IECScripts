8978702b0ceb34075a1d393100e0a8c2ab9cd222
RealTonyShaw <tonyshaw123@hotmail.com>
2019-07-07 12:18:16 +0800

    Revised

---
+++ Scripts/Skill System/SkillTable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTable : iSkillTable
+{
+    // 第 0，1，2 格技能为可切换技能，3 格技能为持续型技能
+    public ISkillCell[] SkillCell { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public void Init()
+    {
+        //EventMgr.KeyDownEvent.OnTrigger();
+    }
+
+    public int SwitchCell()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
---
+++ Scripts/Skill System/iSkillTable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface iSkillTable
+{
+    // 第 0，1，2 格技能为可切换技能，3 格技能为持续型技能
+    ISkillCell[] SkillCell { get; set; }
+
+    int SwitchCell();
+}
