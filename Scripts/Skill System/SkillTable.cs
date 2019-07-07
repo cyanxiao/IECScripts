aef96ae465be388e624570a8f917b0ae9f74ddd3
RealTonyShaw <tonyshaw123@hotmail.com>
2019-07-07 22:19:11 +0800

    Update Skill Table

--- Scripts/Skill System/SkillTable.cs
+++ Scripts/Skill System/SkillTable.cs
@@ -2,19 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillTable : iSkillTable
+public class SkillTable : ISkillTable
 {
     // 第 0，1，2 格技能为可切换技能，3 格技能为持续型技能
-    public ISkillCell[] SkillCell { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public ISkillCell[] SkillCells = new SkillCell[4];
+    private int currentSkillNum = 1;
 
-    public void Init()
+    public void Init(Unit caster, Transform spawnTransform)
     {
-        //EventMgr.KeyDownEvent.OnTrigger();
+        for (int i = 0; i < 4; i++)
+        {
+            SkillCells[i] = new SkillCell();
+            SkillCells[i].Init(caster, spawnTransform);
+        }
+
+        EventMgr.KeyDownEvent.AddListener(SwitchCell);
     }
 
-    public int SwitchCell()
+    public void SwitchCell(EventMgr.KeyDownEventInfo info)
     {
-        throw new System.NotImplementedException();
+        switch (info.keyCode)
+        {
+            case KeyCode.Alpha1:
+                currentSkillNum = 1;
+                Debug.Log("1");
+                break;
+            case KeyCode.Alpha2:
+                currentSkillNum = 2;
+                Debug.Log("2");
+                break;
+            case KeyCode.Alpha3:
+                currentSkillNum = 3;
+                Debug.Log("3");
+                break;
+        }
     }
 
+    public int GetCurrentSkill()
+    {
+        return currentSkillNum;
+    }
+
+
+}
+
+public partial class GameCtrl
+{
+    private void CheckInputForSkillTable()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EventMgr.KeyDownEvent.OnTrigger(new EventMgr.KeyDownEventInfo(KeyCode.Alpha1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EventMgr.KeyDownEvent.OnTrigger(new EventMgr.KeyDownEventInfo(KeyCode.Alpha2));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EventMgr.KeyDownEvent.OnTrigger(new EventMgr.KeyDownEventInfo(KeyCode.Alpha3));
+        }
+    }
 }
