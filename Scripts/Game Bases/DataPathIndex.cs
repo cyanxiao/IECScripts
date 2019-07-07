116f07a92ece85bfd4ce4e398df299d32e45bc8a
1170300303 <indv.panh@gmail.com>
2019-07-07 13:01:52 +0800

    remove .meta & .cs.mata

--- Scripts/Game Bases/DataPathIndex.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "My Data/Data Path Index")]
-public class DataPathIndex : ScriptableObject
-{
-    public string[] paths;
-}
--- Scripts/SceneInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneInfo : MonoBehaviour
-{
-    public GameObject[] objs;
-}
