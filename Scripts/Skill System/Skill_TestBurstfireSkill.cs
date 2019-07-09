2e242c90feeb5633050ec85e10963d1451bf5826
RealTonyShaw <tonyshaw123@hotmail.com>
2019-07-09 16:45:19 +0800

    连射型技能和单发技能 Update

---
+++ Scripts/Skill System/Skill_TestBurstfireSkill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_TestBurstfireSkill : AbstractBurstfireSkill
+{
+    protected override void LoadData()
+    {
+        Data = Gamef.LoadSkillData(SkillName.TestBurstfireSkill);
+    }
+
+    protected override void Shoot()
+    {
+        Debug.Log("Release a single burst fire skill.");
+    }
+}
---
+++ Scripts/Skill System/Skill_TestStrafeSkill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_TestStrafeSkill : AbstractStrafeSkill
+{
+    protected override void LoadData()
+    {
+        Data = Gamef.LoadSkillData(SkillName.TestStrafeSkill);
+    }
+
+    protected override void Shoot()
+    {
+        Debug.Log("Release Strafe Skill");
+    }
+}
