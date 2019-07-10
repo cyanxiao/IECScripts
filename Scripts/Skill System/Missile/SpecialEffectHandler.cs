3fc3d240ce770e321ff01cffa124741105a9d1cb
RealTonyShaw <tonyshaw123@hotmail.com>
2019-07-10 23:10:31 +0800

    Implement Physical Effect Handler

---
+++ Scripts/Skill System/Missile/SpecialEffectHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEffectHandler : ISpecialEffectHandler
+{
+    public void CreateDestroyEffect(Unit caster, Missile missile, GameObject prefab)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CreateSpawnEffect(Unit caster, Missile missile, GameObject prefab)
+    {
+        throw new System.NotImplementedException();
+    }
+}
