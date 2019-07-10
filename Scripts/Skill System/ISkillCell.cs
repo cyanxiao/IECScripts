69a312122db07325ead246e637c83a1a1fc06efc
FANTOME <indv.panh@gmail.com>
2019-07-10 11:32:17 +0800

    fix bugs & prompt
    修复了连射型技能无法正确停止的问题；
    为ISkillCell增加了强制停止施法的方法；
    注意！需要修改SkillTable中Switch方法，在切换技能时正确停止施法。

--- Scripts/Skill System/ISkillCell.cs
+++ Scripts/Skill System/ISkillCell.cs
@@ -14,16 +14,20 @@ public interface ISkillCell
     /// <param name="spawnTransform">投掷物的出生点</param>
     void Init(Unit caster, Transform spawnTransform);
     /// <summary>
-    /// 当鼠标左键被按下
+    /// 当鼠标左键被按下。
     /// </summary>
     void OnMouseButtonDown();
     /// <summary>
-    /// 当鼠标左键被松开
+    /// 当鼠标左键被松开。
     /// </summary>
     void OnMouseButtonUp();
     /// <summary>
-    /// 将当前的技能槽位中的技能设置为skill
+    /// 将当前的技能槽位中的技能设置为skill。
     /// </summary>
     /// <param name="skill">技能</param>
     void SetSkill(ISkill skill);
+    /// <summary>
+    /// 强制停止施法。
+    /// </summary>
+    void ForceToStopCasting();
 }
